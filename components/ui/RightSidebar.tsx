35ffbf8abdacef369e51912922184b28140c460c
rockuutree <vurya22@gmail.com>
2024-07-21 21:22:32 -0700

    svg

--- components/ui/RightSidebar.tsx
+++ components/ui/RightSidebar.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Avatar, Flex, Text, TableRoot, TableBody, TableRow, TableCell, Container } from 'frosted-ui';
+import { Avatar, Flex, Text, TableRoot, TableBody, TableRow, TableCell, Container, SkeletonAvatar } from 'frosted-ui';
+import { ChevronLeft } from 'lucide-react';
+import Image from 'next/image';
 
 const RightSidebar: React.FC = () => {
   const users = [
@@ -16,13 +18,32 @@ const RightSidebar: React.FC = () => {
     { name: 'Rocktree', handle: 'rocktree', src: '/rocktree.jpeg' },
     { name: 'Cameron', handle: 'wh0p', src: '/cameron.png' },
     { name: 'Steven', handle: '.whop', src: '/delmo.png' },
-    { name: 'Delmo', handle: '_delmo', src: '/steven.png' },
-    { name: 'Jordan', handle: '01101010', src: '/jordan.png' },
   ];
 
   return (
-    <Container >
-      <Text weight="bold" mt="5" mb="4" className="text-white p-4">User Directory</Text>
+    <Container>
+      <Flex align="center" mt="5" mb="4" className="text-white p-4">
+        <SkeletonAvatar color="gray" size="2" style={{ position: 'relative' }}>
+        <div style={{
+                  position: 'absolute',
+                  top: '50%',
+                  left: '25%',
+                  transform: 'translate(-50%, -50%)',
+                  width: '24px',
+                  height: '24px'
+                }}>
+      <Image 
+          src='/notif.svg' 
+          alt='notif'
+          layout="fill"
+          objectFit="contain"
+          style={{ transform: 'rotate(180deg)' }}
+          className="cursor-pointer mr-6"
+        />
+        </div>
+        </SkeletonAvatar>
+        <Text weight="bold">User Directory</Text>
+      </Flex>
       <TableRoot size="2" variant="ghost">
         <TableBody>
           {users.map((user, index) => (
@@ -35,7 +56,6 @@ const RightSidebar: React.FC = () => {
                   <Text size="2" className="text-white">{user.name}</Text>
                   <Text size="1" className="text-white">@{user.handle}</Text>
                 </Flex>
-                
               </TableCell>
             </TableRow>
           ))}
